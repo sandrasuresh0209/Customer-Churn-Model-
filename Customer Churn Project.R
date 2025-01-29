fe48a61c481434e259014a81ac35260d708cb9d1
Sandra Suresh <149331431+sandrasuresh0209@users.noreply.github.com>
2025-01-29 14:35:23 -0800

    Update Customer Churn Project.R

--- Customer Churn Project.R
+++ Customer Churn Project.R
@@ -18,14 +18,16 @@ head(Churn)
 #Check for missing values 
 which(is.na(Churn))
 
-#dimensions of dataset
+#Dimensions of dataset
 dim(Churn) #10,0000 observations with 12 columns
+
 #Summarize data 
 summary(Churn) 
 view(Churn)
 
 #Sum of customer churn 
 sum(Churn$churn)
+
 #put in table customer churn 
 count_churn <- table(Churn$churn)
 count_churn
@@ -37,6 +39,7 @@ DFChurn
 #Change row names 
 names(DFChurn) <- c("Churn","Count")
 DFChurn
+
 #If 0 then Yes to Churn 
 DFChurn$Churn <- ifelse(DFChurn$Churn == 0, "Yes", "No")
 
@@ -50,10 +53,8 @@ ggplot(DFChurn, aes(x="", y=Count, fill = Churn)) +
   theme_void()
 
 #Check distribution of credit_score (Histogram)
-
 Churn$credit_score <- as.numeric(Churn$credit_score)
 hist(Churn$credit_score)
-
 
 #Check distribution of Age
 Churn$age <- as.numeric(Churn$age)
@@ -72,7 +73,7 @@ ggplot(Churn, aes(x= churn, fill = country))+
   labs(title = "Churn Distribution by Country", x = "Churn")
 theme_minimal()
 
-# plot box plots to see distributions
+#plot box plots to see distributions
 Churn[, names(Churn) %in% c('age', 'balance', 'credit_score', 'estimated_salary')] %>%
   gather() %>%
   ggplot(aes(value)) +
@@ -80,17 +81,13 @@ Churn[, names(Churn) %in% c('age', 'balance', 'credit_score', 'estimated_salary'
   geom_boxplot() +
   theme(axis.text.x = element_text(size = 7, angle=90), axis.text.y = element_text(size = 7))
 
-
+#Check which variables seem to be correlated to churn 
 ggpairs(data=Churn, columns=2:12, title="Churn Data")
 #product number & credit score seem to be correlated 
 #balance & credit score seem to be correlated
 
+#Data Transformation 
 
-
-```
-
-3. Data Transformation 
-```{r}
 #Create factor variables for categorical variables
 Churn$geodata = factor(Churn$country, labels=c("France","Germany","Spain"))
 Churn$gender = factor(Churn$gender, labels=c("Male","Female"))
@@ -101,18 +98,19 @@ Churn$credit_score = log(Churn$credit_score)
 Churn$balance = log(Churn$balance)
 Churn[Churn$balance == -Inf, 'balance'] <- 0
 
-
 #Scale Data 
-churn_scale_0to1 <- function(x) {                           
+churn_scale_0to1 <- function(x){                           
   (x - min(x)) / (max(x) - min(x))
 }
 Churn$age = churn_scale_0to1(Churn$age)
 Churn$credit_ccore = churn_scale_0to1(Churn$credit_score)
 Churn$balance = churn_scale_0to1(Churn$balance)
 Churn$estimated_salary = churn_scale_0to1(Churn$estimated_salary)
+
+#Check data
 head(Churn, 5)
 
-#Build Model 
+#Build ML Model : Classification
 trainIndex <- createDataPartition(Churn$churn, p = 0.8, list = FALSE, times = 1)
 training_data <- Churn[ trainIndex,]
 testing_data  <- Churn[-trainIndex,]
@@ -121,23 +119,28 @@ testing_data  <- Churn[-trainIndex,]
 prop.table(table(training_data$churn))
 prop.table(table(testing_data$churn))
 
-#Lets see which features are important to predicting customer retention 
+#Lets see which features are important to predicting customer retention - logistic regression
 LogModel <- glm(churn ~., data = training_data, family = "binomial")
 summary(LogModel)
+
+#we can drop variables with (p values > 0.05)
 
 LogModel <- glm(formula = churn ~ credit_score + country + gender + age + balance + products_number + active_member, family = "binomial", data = training_data)
 summary(LogModel)
 
-#VIF Scores 
+#Check VIF Scores to see multicollinearity 
 vif(LogModel)
 
+
+#Check model accuracy on testing data
 prediction <- predict(LogModel,testing_data,type="response")
 cutoff <- ifelse(prediction>=0.50, 1,0)
 cm <- confusionMatrix(as.factor(testing_data$churn),as.factor(cutoff),positive ='1')
 cm
 
-#SVM Model 
 
+
+#SVM Model 
 training_data$churn <- as.factor(training_data$churn)
 
 svm.model <- train(
@@ -148,5 +151,4 @@ svm.model <- train(
   preProcess = c("center", "scale")
 )
 svm.model
-
 
